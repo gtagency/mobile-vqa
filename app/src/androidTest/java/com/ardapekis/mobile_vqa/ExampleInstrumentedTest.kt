0f952257ca469ba15eda07436f9dddef77a63a95
yatyrio <abhinavk99@gmail.com>
2018-02-13 22:22:52 -0500

    Added Android Studio Project

---
+++ app/src/androidTest/java/com/ardapekis/mobile_vqa/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.ardapekis.mobile_vqa
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.ardapekis.mobile_vqa", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/ardapekis/mobile_vqa/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ardapekis.mobile_vqa
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
